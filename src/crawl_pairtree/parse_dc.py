0043744b00574d0834c05f710578399c909b9123
svirgilgood <svirgilgoodwin@gmail.com>
2026-06-01 12:49:12 -0600

    adding the dc terms parsing for files of that class

---
+++ src/crawl_pairtree/parse_dc.py
@@ -0,0 +1,29 @@
+from rdflib import Graph, term
+
+from pyoxigraph import Store, NamedNode, Literal, Quad
+from pathlib import Path
+
+from typing import List
+
+
+def parse_dc(file: Path, id_node: NamedNode, store: Store):
+    """
+    takes the full path to the file
+    """
+    graph = Graph()
+    graph.parse(file, format="xml")
+
+    for _, pred, obj in graph.triples((None, None, None)):
+        predicate = NamedNode(str(pred))
+        match obj:
+            case term.URIRef():
+                obj_node = NamedNode(str(obj))
+            case term.Literal():
+                obj_node = Literal(str(obj))
+            case term.BNode():
+                continue
+            case _:
+                # This maybe should be recursive
+                obj_node = Literal(str(obj))
+
+        store.add(Quad(id_node, predicate, obj_node))
